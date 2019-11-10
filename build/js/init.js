15b0048fd9cd307398d44ccea5bd336639457be5
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-11-10 16:13:16 +0100

    WIP new forms

--- build/js/init.js
+++
@@ -1,2 +0,0 @@
-"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".sidenav");M.Sidenav.init(e,{edge:"right",draggable:!0})}),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".tooltipped");M.Tooltip.init(e,{})});
-//# sourceMappingURL=init.js.map
